c52c4c802361dc5f9ef823e7325c690257055ed6
skymins04 <skymin0417@gmail.com>
2022-01-06 21:12:19 +0900

    solved 1759

--- test.py
+++ test.py
@@ -1,9 +1,39 @@
-arr=input().split('-')
-min = 0
-for i in arr[0].split('+'):
-    min += int(i.lstrip('0'))
-arr.pop(0)
-for i in arr:
-    for j in i.split('+'):
-        min -= int(j.lstrip('0'))
-print(min)
+L,C = map(int, input().split())
+A = []
+B = []
+for c in input().split():
+    if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
+        A.append(c)
+    else:
+        B.append(c)
+caseA = []
+caseB = []
+result = []
+
+def dfsA(_i, _l):
+    if len(_l) > 0:
+        caseA.append(_l[:])
+    for i in range(_i, len(A)):
+        _l += [A[i]]
+        dfsA(i+1, _l)
+        _l.pop(len(_l)-1)
+
+def dfsB(_i, _l):
+    if len(_l) > 1:
+        caseB.append(_l[:])
+    for i in range(_i, len(B)):
+        _l += [B[i]]
+        dfsB(i+1, _l)
+        _l.pop(len(_l)-1)
+
+dfsA(0, [])
+dfsB(0, [])
+
+for a in caseA:
+    for b in caseB:
+        if len(b) == L-len(a):
+            result.append(''.join(sorted(a+b)))
+result = sorted(result)
+
+for i in result:
+    print(i)
